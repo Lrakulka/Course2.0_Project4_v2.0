d739b99ce5732e949751548b669ad3d8a47c994d
Lrakulka <olecsandr32@gmail.com>
2016-07-28 15:04:50 -0700

    Low Battery charge

--- src/main/java/com/epam/configuration/SpringSecurityConfig.java
+++ src/main/java/com/epam/configuration/SpringSecurityConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests().antMatchers("/admin/**")
-			.access("hasRole('ROLE_ADMIN')").and().formLogin()
+			.access("hasRole('admin')").and().formLogin()
 			.loginPage("/login").failureUrl("/login?error")
 				.usernameParameter("username")
 				.passwordParameter("password")
--- src/main/java/com/epam/controller/MainController.java
+++ src/main/java/com/epam/controller/MainController.java
@@ -1,7 +1,21 @@
 package com.epam.controller;
 
+import com.epam.model.User;
+import com.epam.service.BillService;
+import com.epam.service.CardService;
+import com.epam.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 /**
  * Created by fg on 7/27/2016.
@@ -9,8 +23,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping("/*")
-    public String init() {
-        return "index";
+    private static final Logger logger = Logger.getLogger(MainController.class);
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CardService cardService;
+    @Autowired
+    private BillService billService;
+
+    /**
+     * Default page.
+     * @return model of welcome page
+     */
+    @RequestMapping(value = "/**")
+    public String welcome() {
+        return "welcome";
     }
 }
