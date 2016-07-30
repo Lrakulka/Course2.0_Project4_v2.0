f8024064257db5cd104b1029838d0b458809f258
Lrakulka <olecsandr32@gmail.com>
2016-07-30 13:29:12 -0700

    Added client role to spring access security filter.

--- src/main/java/com/epam/configuration/SpringSecurityConfig.java
+++ src/main/java/com/epam/configuration/SpringSecurityConfig.java
@@ -27,14 +27,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers("/admin/**")
-			.access("hasRole('ROLE_ADMIN')").and().formLogin()
-			.loginPage("/login").failureUrl("/login?error")
+		http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/client/**").hasRole("CLIENT")
+            .and().formLogin()
+			    .loginPage("/login").failureUrl("/login?error")
 				.usernameParameter("username")
 				.passwordParameter("password")
-				.and().logout().logoutSuccessUrl("/login?logout")
-				.and().exceptionHandling().accessDeniedPage("/403");
+            .and().logout()
+                .logoutSuccessUrl("/login?logout")
+            .and().exceptionHandling()
+                .accessDeniedPage("/403");
 	}
 	
 	@Bean
